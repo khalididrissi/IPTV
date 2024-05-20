1c52edcae72586ad556837b699ccb982d4525b40
KHALID IDRISSI <150524142+khalididrissi@users.noreply.github.com>
2024-05-20 20:11:05 +0100

    Create Ads.js

---
+++ Ads.js
@@ -0,0 +1,65 @@
+ function blockElementsByClass(className) {
+            var elements = document.getElementsByClassName(className);
+
+            // Remove elements from the DOM
+            while (elements.length > 0) {
+                elements[0].parentNode.removeChild(elements[0]);
+            }
+        }
+
+        blockElementsByClass('logo');
+
+        blockElementsByClass('notranslate');
+
+        blockElementsByClass('navbar-brand');
+
+        blockElementsByClass('box-title');
+
+        blockElementsByClass('tag-mb');
+
+        blockElementsByClass('tabsbox');
+
+        blockElementsByClass('footermb');
+
+        blockElementsByClass('div-gpt-ad-8176806-9');
+        blockElementsByClass('div-gpt-ad-8176806-8');
+
+        blockElementsByClass('');
+
+        blockElementsByClass('');
+
+        blockElementsByClass('');
+
+        blockElementsByClass('');
+
+        blockElementsByClass('');
+
+        blockElementsByClass('');
+
+        blockElementsByClass('');
+
+
+
+
+
+        // Remove elements by ID
+        var elementsToRemoveById = [
+            'id-custom_banner',
+            'google_ads_iframe_/22654869840/apl_1__container__',
+            'google_ads_iframe_/22893379435/apl_0__container__',
+            'dontfoid', // <-- Remove this element
+            'gpt_unit_/7047,22643491855/apl/anchor/anchortop_0'
+        ];
+
+        elementsToRemoveById.forEach(function(elementId) {
+            var element = document.getElementById(elementId);
+            if (element) {
+                element.remove();
+            }
+        });
+
+        // Remove additional aplvideo elements by class name
+        var aplvideoDivs = document.getElementsByClassName('aplvideo');
+        while (aplvideoDivs.length > 0) {
+            aplvideoDivs[0].remove();
+        }
