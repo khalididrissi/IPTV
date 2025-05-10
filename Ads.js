523bab5d30059b88eeb0e61f3d3ae13308393ad5
KHALID IDRISSI <150524142+khalididrissi@users.noreply.github.com>
2025-05-10 23:14:06 +0100

    Update Ads.js

--- Ads.js
+++ Ads.js
@@ -1,61 +1,69 @@
 function blockElementsByClass(className) {
-    var elements = document.getElementsByClassName(className);
+var elements = document.getElementsByClassName(className);
 
-    // Remove elements from the DOM
-    while (elements.length > 0) {
-        elements[0].parentNode.removeChild(elements[0]);
+        // Remove elements from the DOM
+        while (elements.length > 0) {
+            elements[0].parentNode.removeChild(elements[0]);
+        }
     }
-}
 
-blockElementsByClass('AlbaSport_header');
-blockElementsByClass('LIVEVIPlogo');
-blockElementsByClass('aspwp-e3lan e3lan-before_table_content');
-blockElementsByClass('aspwp-e3lan e3lan-after_table_content');
-blockElementsByClass('site-info');
-blockElementsByClass('widget-visible');
-blockElementsByClass('alba-logo mr-4 d-inline-flex align-items-center');
-blockElementsByClass('aplr-fxd-bnr');
-blockElementsByClass('aspwp-e3lan e3lan-after_match_card');
-blockElementsByClass('aplr-notic');
-blockElementsByClass('aspwp-e3lan e3lan-before_match_info');
-blockElementsByClass('aspwp-e3lan e3lan-after_match_info');
-blockElementsByClass('aplr-bnr-inner');
-blockElementsByClass('menu-item menu-item-type-custom menu-item-object-custom menu-item-847');
-blockElementsByClass('AlbaSport-main box');
-blockElementsByClass('notic-telegram');
+    blockElementsByClass('AlbaSport_header');
+    blockElementsByClass('LIVEVIPlogo');
 
-// Remove elements by ID
-var elementsToRemoveById = [
-    'id-custom_banner',
-    'google_ads_iframe_/22654869840/apl_1__container__',
-    'google_ads_iframe_/22893379435/apl_0__container__',
-    'dontfoid',
-    'gpt_unit_/7047,22643491855/apl/anchor/anchortop_0',
-    'aplr-notic-inner',
-    'aplr-notic',
-    'aplr-fixedban',
-    'atContainer-a2d918dabce8894ebd1006e873d52989'
-];
+    blockElementsByClass('aspwp-e3lan e3lan-before_table_content');
 
-elementsToRemoveById.forEach(function(elementId) {
-    var element = document.getElementById(elementId);
-    if (element) {
-        element.remove();
+    blockElementsByClass('aspwp-e3lan e3lan-after_table_content');
+
+    blockElementsByClass('site-info');
+
+    blockElementsByClass('widget-visible');
+
+    blockElementsByClass('alba-logo mr-4 d-inline-flex align-items-center');
+
+    blockElementsByClass('aplr-fxd-bnr');
+
+    blockElementsByClass('aspwp-e3lan e3lan-after_match_card');
+
+    blockElementsByClass('aplr-notic');
+
+    blockElementsByClass('aspwp-e3lan e3lan-before_match_info');
+
+    blockElementsByClass('aspwp-e3lan e3lan-after_match_info');
+
+    blockElementsByClass('aplr-bnr-inner');
+
+    blockElementsByClass('menu-item menu-item-type-custom menu-item-object-custom menu-item-847');
+
+    blockElementsByClass('AlbaSport-main box');
+
+    blockElementsByClass('notic-telegram');
+
+
+
+
+
+    // Remove elements by ID
+    var elementsToRemoveById = [
+        'id-custom_banner',
+        'google_ads_iframe_/22654869840/apl_1__container__',
+        'google_ads_iframe_/22893379435/apl_0__container__',
+        'dontfoid',
+        'gpt_unit_/7047,22643491855/apl/anchor/anchortop_0',
+        'aplr-notic-inner',
+        'aplr-notic',
+        'aplr-fixedban',
+        'atContainer-a2d918dabce8894ebd1006e873d52989'
+    ];
+
+    elementsToRemoveById.forEach(function(elementId) {
+        var element = document.getElementById(elementId);
+        if (element) {
+            element.remove();
+        }
+    });
+
+    // Remove additional aplvideo elements by class name
+    var aplvideoDivs = document.getElementsByClassName('aplr-fixedban');
+    while (aplvideoDivs.length > 0) {
+        aplvideoDivs[0].remove();
     }
-});
-
-// Remove additional aplvideo elements by class name
-var aplvideoDivs = document.getElementsByClassName('aplr-fixedban');
-while (aplvideoDivs.length > 0) {
-    aplvideoDivs[0].remove();
-}
-
-// Remove the navigation with class 'LIVEVIPheader'
-function blockNavByClass(className) {
-    var navElement = document.querySelector('.' + className); // Select the nav element
-    if (navElement) {
-        navElement.remove(); // Remove it from the DOM
-    }
-}
-
-blockNavByClass('LIVEVIPheader');
